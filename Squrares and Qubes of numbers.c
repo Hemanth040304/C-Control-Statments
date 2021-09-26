2e8e032ef4c9555adb3d122828aabc00e210d0cc
Hemanth040304 <91045353+Hemanth040304@users.noreply.github.com>
2021-09-27 03:56:07 +0530

    Update Squrares and Qubes of numbers.c

--- Squrares and Qubes of numbers.c
+++ Squrares and Qubes of numbers.c
@@ -2,10 +2,16 @@
 void main()
 {
 	int number1,number2;
+	printf("Enter two numbers.\n");
 	scanf("%d%d",&number1,&number2);
-	while(number1<=number2) 
+	if(number1<=number2)
 	{
-	    printf("%d\t%d\t%d\n ",number1,number1*number1,number1*number1*number1);
-		number1++;	
+		while(number1<=number2) 
+	        {
+	          printf("%d\t%d\t%d\n ",number1,number1*number1,number1*number1*number1);
+		  number1++;	
+	        }
 	}
+	else
+		printf("number1 should be less than number2");
 }
