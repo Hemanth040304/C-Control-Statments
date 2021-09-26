6690a56ad12e5e3699f57dc2b975a144c23dd950
Hemanth040304 <91045353+Hemanth040304@users.noreply.github.com>
2021-09-27 00:14:28 +0530

    Add files via upload

---
+++ Squrares and Qubes of numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int number1,number2;
+	scanf("%d%d",&number1,&number2);
+	while(number1<=number2) 
+	{
+	    printf("%d\t%d\t%d\n ",number1,number1*number1,number1*number1*number1);
+		number1++;	
+	}
+	while(number1>=number2)
+	{
+		printf("number1 should me Less than number2");
+		break;
+	}
+}
