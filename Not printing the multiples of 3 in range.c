00cdff085ab23a29147f9e40ad2c9b66fb638b0f
Hemanth040304 <91045353+Hemanth040304@users.noreply.github.com>
2021-09-29 17:26:07 +0530

    Add files via upload

---
+++ Not printing the multiples of 3 in range.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int number1,number2;
+	printf("Enter two numbers.\n");
+	scanf("%d%d",&number1,&number2);
+	while(number1<=number2)
+	{
+		if(number1%3!=0)
+		{
+			printf("%d ",number1);
+		}
+		number1++;
+	}
+}
