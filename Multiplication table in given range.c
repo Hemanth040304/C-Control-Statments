3753e662ffeceb09be56f258ac86ee24db560fce
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2021-10-09 13:50:19 +0530

    Add files via upload

---
+++ Multiplication table in given range.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int number1,number2,number3;
+	printf("Enter range of tables.\n");
+	scanf("%d%d",&number1,&number2);
+	while(number1<=number2)
+	{
+		number3=1;
+		while(number3<=12)
+		{
+			printf("%d * %d = %d\n",number1,number3,number1*number3);
+		    number3++;
+		}
+		printf("\n");
+		number1++;
+		getch();
+	}
+}
