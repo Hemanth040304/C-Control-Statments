9c6ec1482b3fba973b852b3ff869c2658d3e26c8
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2021-10-10 20:43:00 +0530

    Add files via upload

---
+++ Triangle 10.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+ //					*****
+ //					****
+ //					***
+ //					**
+ //	Sample Result	*
+ //					**
+ //					***
+ //					****
+ //					*****
+{
+	int i=1,j;
+	while(i<=5)
+	{
+		j=5;
+		while(j>=i)
+		{
+			printf("*");
+			j--;
+		}
+		printf("\n");
+		i++;
+	}
+	while(i<=9)
+	{
+		j=5;
+		while(j<=i)
+		{
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
