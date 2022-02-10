c7b3c2fcbfa553c01852602cbb07e96250907829
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-02-10 10:37:39 +0530

    Add files via upload

---
+++ Maximum sum of 4 sides of a number in matrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum,max=0,i,j;
+	scanf("%d",&n);
+	int arr[n][n];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=0;
+		for(j=0;j<n;j++)
+		{
+			if(i!=0 && j!=0 && i!=n-1 && j!=n-1)
+			{
+				sum=arr[i-1][j]+arr[i][j-1]+arr[i+1][j]+arr[i][j+1];
+			}
+		}
+		if(max<sum)
+		max=sum;
+	}
+	printf("%d",sum);
+}
